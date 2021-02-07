af1671a5835a9812613656f82251ca76bef3e0be
umang <umangpatel13998@gmail.com>
2021-02-07 10:09:41 +0530

    scheduler

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReCaptchaV3Service } from 'ng-recaptcha';
-import { MtabCaptchaComponent } from './main/mtab-captcha/mtab-captcha.component';
 
 const routes: Routes = [
   {
-    path: 'captcha',
-    component: MtabCaptchaComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MtabCaptchaModule } from './main/mtab-captcha/mtab-captcha.module';
+import { MtabSchedulerModule } from './scheduler/scheduler.module';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { SchedulerModule } from 'angular-calendar-scheduler';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    MtabCaptchaModule
+    MtabSchedulerModule,
+  //   CalendarModule.forRoot({
+  //     provide: DateAdapter,
+  //     useFactory: adapterFactory,
+  //   }),
+  // SchedulerModule.forRoot({ locale: 'en', headerDateFormat: 'daysRange' }),
   ],
   providers: [],
   bootstrap: [AppComponent]
